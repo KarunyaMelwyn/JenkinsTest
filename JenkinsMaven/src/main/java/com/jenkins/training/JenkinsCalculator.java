b0f1ed1e4ebe72334da319e22b8b3767f9ee0f28
MOSES VASANTHAN <karunyaanand2696@gmail.com>
2024-04-19 21:48:48 +0530

    changed argument

--- JenkinsMaven/src/main/java/com/jenkins/training/JenkinsCalculator.java
+++ JenkinsMaven/src/main/java/com/jenkins/training/JenkinsCalculator.java
@@ -1,11 +1,11 @@
 package com.jenkins.training;
 public class JenkinsCalculator {
 
-    public int addNumbers(int num1,int num2){
-        return num1+num2;
+    public int addNumbers(int number1,int number2){
+        return number1+number2;
     }
-    public int subNumbers(int num1,int num2){
-        return num1-num2;
+    public int subNumbers(int number1,int number2){
+        return number1-number2;
     }
     public static void main(String[] args){
 
